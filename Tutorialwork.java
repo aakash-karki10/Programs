47225458f7434de72cac5776a67cbe24411ebe89
Pyaroj Karki <aakashkarki2023@gmail.com>
2025-11-21 09:23:18 +0545

    Initial sharing of project

---
+++ Tutorialwork.java
@@ -0,0 +1,55 @@
+
+/**
+ * Write a description of class Tutorialwork here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Tutorialwork
+{
+    int qty; // instance variable
+    static int rrt; //static variable
+    public static void main(String[] args){
+        int age=18; // local variable
+        System.out.println(age);
+        System.out.println(Tutorialwork.rrt);
+        
+        // Implict Typecasting
+        double dtr= age;
+        System.out.println(dtr);
+        
+        // explicit Typecasting
+         double db= 10.09;
+         int itr= (int)db;
+         System.out.println(itr);
+         
+         // Exceptions in arithmetic
+         
+         
+        byte b1= 10;
+        byte b2= 11;
+        
+        byte sum= (byte)(b1+b2);
+        
+        
+        // short,char
+        
+        
+        
+        
+        //Minimum, maximum, size, bytes
+        
+        
+        System.out.println(Byte.MAX_VALUE); //maximum value
+        System.out.println(Byte.MIN_VALUE); // minimium value
+        System.out.println(Byte.SIZE); //bits
+        System.out.println(Byte.BYTES); //bytes
+        
+        // Escape Sequence
+        System.out.println("Hello\nWorld");
+        System.out.println("Hamro\tNepal");
+        System.out.println(""
+        
+        
+    }
+}
